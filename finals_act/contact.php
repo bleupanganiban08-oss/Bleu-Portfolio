c913a8438be5c24b1f004835f7cd4ee5c8e1841a
Bleu Inigo Panganiban <bleupanganiban08@gmail.com>
2026-05-01 17:53:38 +0800

    Add files via upload

---
+++ finals_act/contact.php
@@ -0,0 +1,24 @@
+<?php
+include 'header.php';
+ 
+echo '<h1>Bleu Inigo Panganiban</h1>';
+echo '<p> GUAGUA NATIONAL COLLEGES STUDENT</p>';
+echo '<p> 🌐 | 📊</p>';
+echo '<p>┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉</p>';
+echo '<h1>Contact Me</h1>';
+echo '<p>You can reach me at ⤵⤵⤵⤵⤵</p>';
+echo '<ul>';
+echo '<li>Email: bleupanganiban08@gmail.com</li>';
+echo '<li>Phone: +63 993 6825 807</li>';
+echo '<li>Home Address: Baranggay Sto. Tomas, Lubao, Pampanga </li>';
+echo '</ul>';
+echo '<p><i>Note: This is a demo project. No form submission or database connection is implemented.</i></p>';
+echo '<style>
+    h1 { font-size: 24px; margin-left: 10px; }
+    p { font-size: 16px; margin-left: 10px; }
+    ul { list-style-type: disc; padding-left: 20px; margin-left: 10px;}
+    li { margin-bottom: 5px; }
+</style>';
+ 
+include 'footer.php';
+?>
---
+++ finals_act/footer.php
@@ -0,0 +1,8 @@
+</div>
+<footer style="text-align: center; margin-top: 20px; color: #777;">
+<?php
+    echo '<p>&copy; 2026 made by Bleu Inigo Panganiban. All rights reserved.</p>';
+    ?>
+</footer>
+</body>
+</html>
